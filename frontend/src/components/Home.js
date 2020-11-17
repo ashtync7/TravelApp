54915d2739eb00113f836884019451dd6a072f0a
Jada Woody <JadaWoody@MacBook-Pro-16.local>
2020-11-17 18:02:36 -0500

    tuesdayNight

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,7 +3,10 @@ import React from 'react';
 function Home(props) {
     return (
         <div>
-            
+            {/* menu/navbar */}
+            {/* carousel with nice pics */}
+            {/* home page includes: basic info - about us type info, customer testimonials/reviews slider?!, more pictures, footer w/fake address, phone number, and social media handles */}
+
         </div>
     );
 }
