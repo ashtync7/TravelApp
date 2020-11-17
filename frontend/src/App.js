9e9828da9350760ac7e2d699bc626d674f2a62d4
Ashtyn Ce <ashtyncza@gmail.com>
2020-11-17 18:42:50 -0500

    ashtynnnnn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,22 @@ import React, { Component, Fragment, useState, useEffect } from "react";
 import { Switch, Route, NavLink, useHistory } from "react-router-dom";
 import Home from './components/Home'
 const App = () => {
-  
-  return(
+
+
+
+
+  return (
     <div>
+      <ul class="menu">
+        <li title="home"><a href="#" class="home">home</a></li>
+        <li title="search"><a href="#" class="search">search</a></li>
+        <li title="pencil"><a href="#" class="pencil">pencil</a></li>
+        <li title="about"><a href="#" class="active about">about</a></li>
+        <li title="travel resources"><a href="#" class="archive">archive</a></li>
+        <li title="contact"><a href="#" class="contact">contact</a></li>
+      </ul>
 
-      <Switch>
-        <Route exact path="/" render={() => <Home/> } />
-      </Switch>
-
-    
-      
-    </div>
+    </div >
   )
 
 }
