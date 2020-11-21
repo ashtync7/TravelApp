6421f6e2a4126c45d76c16d14c60f928dd7640dc
Jada Woody <JadaWoody@MacBook-Pro-16.local>
2020-11-21 09:33:20 -0500

    sat933

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ const App = () => {
     <div>
       <ul class="menu">
         <li title="home"><a href="#" class="home">home</a></li>
-        <li title="destinations"><a href="#" class="fa-map-marker-alt">destinations</a></li>
+        <img src="/images/icons8-map-marker-24.png" alt="map icon"/><li title="destinations"><a href="#" class="fa-map-marker-alt">destinations</a></li>
         <li title="pencil"><a href="#" class="pencil">pencil</a></li>
         <li title="travel resources"><a href="#" class="archive">archive</a></li>
         <li title="contact"><a href="#" class="contact">contact</a></li>
