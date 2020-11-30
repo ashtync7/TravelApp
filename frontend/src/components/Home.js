7315787153227ee9215a5febb8472910e7be3fe9
Ashtyn Ce <ashtyncza@gmail.com>
2020-11-30 16:48:48 -0500

    WTA

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -24,7 +24,7 @@ function Home(props) {
             </div> */}
 
             <div className="homeHeader">
-                <h2>Company Name</h2>
+                <h2>Wanderlust Travel Agency</h2>
                 <h3>Travel Agency Moto</h3>
                 <h4>Sentence of Summary</h4>
             </div>
