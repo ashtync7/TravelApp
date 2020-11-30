9e91c5c1d1b68fac9efb9cd9dfb291d21009c154
Jada Woody <JadaWoody@MacBook-Pro-16.local>
2020-11-30 17:23:30 -0500

    mon500

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -25,7 +25,7 @@ function Home(props) {
 
             <div className="homeHeader">
                 <h2>Wanderlust Travel Agency</h2>
-                <h3>Travel Agency Moto</h3>
+                <img id="glowSlogan" src="images/glowSlogan1.png" alt="Not All Who Wander Are Lost" />
                 <h4>Sentence of Summary</h4>
             </div>
         </div>
