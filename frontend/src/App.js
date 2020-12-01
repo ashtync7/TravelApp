67cf7ca7e05c417fbb15c111456a7b14ca502cb7
Jada Woody <JadaWoody@MacBook-Pro-16.local>
2020-12-01 14:39:14 -0500

    tues240

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,24 @@
 import React, { Component, Fragment, useState, useEffect } from "react";
 import { Switch, Route, NavLink, useHistory } from "react-router-dom";
+import {Link} from 'react-router-dom';
 import Home from './components/Home'
+import Destinations from './components/Destinations';
+import CustomVacations from './components/CustomVacation';
+import TravelResources from './components/TravelResources';
+import ContactUs from './components/Contact';
+
 const App = () => {
 
   return (
     <div>
       <nav className="navBar">
         <ul>
-          <img id="logo" src="images/WTAlogo.png"></img>
-          <li>Home</li>
-          <li>Destinations</li>
-          <li>Custom Vacations</li>
-          <li>Travel Resources</li>
-          <li>Contact Us</li>
+        <Link to={`/`}><img id="logo" src="images/WTAlogoVan1.png"></img></Link>
+          <Link to={`/`}><li>Home</li></Link>
+          <Link to={`/Destinations`}><li>Destinations</li></Link>
+          <Link to={`/CustomVacations`}><li>Custom Vacations</li></Link>
+          <Link to={`/TravelResources`}><li>Travel Resources</li></Link>
+          <Link to={`/ContactUs`}><li>Contact Us</li></Link>
         </ul>
       </nav>
       {/* <div>
@@ -27,6 +33,10 @@ const App = () => {
 
       <Switch>
         <Route exact path='/' render={() => <Home />} />
+        <Route exact path='/Destinations' render={() => <Destinations />} />
+        <Route exact path='/CustomVacations' render={() => <CustomVacations />} />
+        <Route exact path='/TravelResources' render={() => <TravelResources />} />
+        <Route exact path='/ContactUs' render={() => <ContactUs />} />
       </Switch>
 
     </div>
