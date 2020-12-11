fa86773531961441122f4ad69eedcd2456620faa
Ashtyn Ce <ashtyncza@gmail.com>
2020-12-11 13:46:26 -0500

    fri145

--- frontend/src/components/TravelResources.js
+++ frontend/src/components/TravelResources.js
@@ -3,8 +3,109 @@ import React from 'react';
 function TravelResources(props) {
     return (
         <div>
-            <h2>Travel Resources Page</h2>
+            <div className="content">
+                <h1 className="section-header">Helpful Resources for Traveling</h1>
+                <div className="section-header-underline"></div>
+                <div className="video-gallery">
+                    <div className="gallery-item">
+                        <img src="https://www.infocusclinical.com/wp-content/uploads/2019/12/MG_1_1_New_York_City-1.jpg" alt="NYC" />
+                        <div className="gallery-item-caption">
+                            <div>
+                                <a href=" https://travel.state.gov/content/travel/en/international-travel.html" target="_blank">
+                                    <h2>International Travel</h2>
+                                </a>
+                                <p>Know the guidelines for travel to your destination, before you go!</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="gallery-item ">
+                        <img src="https://media.timeout.com/images/105718842/image.jpg" alt="Mt. Rainier" />
+                        <div className="gallery-item-caption">
+                            <div>
+                                <a href="https://www.rei.com/" target="_blank">
+                                    <h2>REI Co-Op</h2>
+                                </a>
+                                <p>Get all the gear you'll need!</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="gallery-item">
+                        <img src="https://www.fortunebuilders.com/wp-content/uploads/2014/12/miami-real-estate-investing.jpg" alt="Olympic National Park" />
+                        <div className="gallery-item-caption">
+                            <div>
+                                <a href=" https://expertvagabond.com/travel/" target="_blank">
+                                    <h2>Budget Travel Resources Guide</h2>
+                                </a>
+                                <p> Get travel gear & packing tips to stay on budget!</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="gallery-item">
+                        <img src="https://drivito2.imgix.drizly.com/5f93122b7685d985/86050bb2137e/wa-seattle.jpg?auto=format%2Ccompress&fm=jpeg&q=40&w=1800" alt="Mount St. Helens" />
+                        <div className="gallery-item-caption">
+                            <div>
+                                <a href="https://www.eatright.org/health/lifestyle/travel/8-food-safety-tips-when-traveling-abroad" target="_blank">
+                                    <h2>Food Safety While Traveling</h2>
+                                </a>
+                                <p> 8 Food Safety Tips When Traveling Abroad</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="gallery-item ">
+                        <img src="https://republicanatheists.com/wp-content/uploads/2018/02/Chicago_illinois.jpg" alt="Mt. Rainier" />
+                        <div className="gallery-item-caption">
+                            <div>
+                                <a href="https://www.back-packer.org/30-tips-and-ways-to-stay-safe-while-traveling-the-world-and-south-america/" target="_blank">
+                                    <h2>Travel Safety</h2>
+                                </a>
+                                <p>30 tips to staying safe while you travel!</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="gallery-item ">
+                        <img src="https://a.netzero-usa.com/site/media/Honolulu-Hawaii.png" alt="Mt. Rainier" />
+                        <div className="gallery-item-caption">
+                            <div>
+                                <a href="https://glassofbubbly.com/5-great-ways-to-enjoy-your-travel/" target="_blank">
+                                    <h2>Enjoy Your Travels</h2>
+                                </a>
+                                <p>Tips to help you have the best trip ever!</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="gallery-item ">
+                        <img src="https://files.slack.com/files-pri/T02CQ4EN4-F01GMUM3M8V/ta-camera.jpg" alt="Mt. Rainier" />
+                        <div className="gallery-item-caption">
+                            <div>
+                                <a href="https://expertvagabond.com/best-travel-camera/" target="_blank">
+                                    <h2>Best Cameras for Travel</h2>
+                                </a>
+                                <p> Find the best travel camera for you!</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="gallery-item ">
+                        <img src="https://a.netzero-usa.com/site/media/Honolulu-Hawaii.png" alt="Mt. Rainier" />
+                        <div className="gallery-item-caption">
+                            <div>
+                                <a href="https://all.accor.com/gb/middle-east/magazine/one-hour-one-day-one-week/20-ultimate-travel-bucket-list-ideas-to-do-before-40-17ae1.shtml" target="_blank">
+                                    <h2>For Your Inspiration</h2>
+                                </a>
+                                <p>Read these 20 ultimate travel bucket list items and get inspired for your trip!</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
     );
 }
 
